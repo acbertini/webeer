7779a2e762c147b01d6ef1c72a127831bfa919d1
CarlosFSNeto <carlosneto@outlook.com>
2018-09-19 09:54:18 -0300

    Exclusão dos arquivos do projeto do NetBeans; Adição dos arquivos do projeto feito no Eclipse.

---
+++ src/main/java/br/com/webeer/HelloAppEngine.java
@@ -0,0 +1,27 @@
+package br.com.webeer;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(
+    name = "Hello",
+    urlPatterns = {"/hello"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().println("Beba com moderação - SKOLLLLL!!!\r\n");
+
+  }
+}
